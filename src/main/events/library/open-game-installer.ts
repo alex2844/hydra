9485ef6230ee8aed321b6e78acae248f186c68a8
Alex Smith <alex.smith2844@gmail.com>
2024-10-29 02:34:22 +0500

    support linux

--- src/main/events/library/open-game-installer.ts
+++ src/main/events/library/open-game-installer.ts
@@ -10,7 +10,7 @@ import { generateYML } from "../helpers/generate-lutris-yaml";
 import { getDownloadsPath } from "../helpers/get-downloads-path";
 import { registerEvent } from "../register-event";
 
-const executeGameInstaller = (filePath: string) => {
+const executeGameInstaller = async (filePath: string) => {
   if (process.platform === "win32") {
     shell.openPath(filePath);
     return true;
@@ -20,6 +20,30 @@ const executeGameInstaller = (filePath: string) => {
     exec(`wine "${filePath}"`);
     return true;
   }
+
+	const STEAM_COMPAT_CLIENT_INSTALL_PATH = `${process.env.HOME}/.local/share/Steam`;
+	if (fs.existsSync(STEAM_COMPAT_CLIENT_INSTALL_PATH)) {
+		const [ ver ] = fs.readdirSync(`${STEAM_COMPAT_CLIENT_INSTALL_PATH}/steamapps/common/`).filter(proton => proton.startsWith('Proton '));
+		if (ver) {
+			const STEAM_COMPAT_MOUNTS = `${STEAM_COMPAT_CLIENT_INSTALL_PATH}/steamapps/common/${ver}`;
+			const STEAM_COMPAT_DATA_PATH = `${process.env.HOME}/.config/hydra/compatdata/${gameId}`;
+			const WINEPREFIX = `${STEAM_COMPAT_DATA_PATH}/pfx`;
+			if (!fs.existsSync(`${WINEPREFIX}/drive_c`))
+				fs.mkdirSync(`${WINEPREFIX}/drive_c`, { recursive: true });
+			if (!fs.existsSync(`${WINEPREFIX}/drive_c/Games`))
+				fs.symlinkSync(await getDownloadsPath(), `${WINEPREFIX}/drive_c/Games`);
+			exec(`"${STEAM_COMPAT_MOUNTS}/proton" run "${filePath}"`, {
+				env: {
+					...process.env,
+					STEAM_COMPAT_CLIENT_INSTALL_PATH,
+					STEAM_COMPAT_MOUNTS,
+					STEAM_COMPAT_DATA_PATH,
+					WINEPREFIX
+				}
+			});
+			return true;
+		}
+	}
 
   return false;
 };
@@ -56,7 +80,7 @@ const openGameInstaller = async (
 
   const setupPath = path.join(gamePath, "setup.exe");
   if (fs.existsSync(setupPath)) {
-    return executeGameInstaller(setupPath);
+    return await executeGameInstaller(setupPath);
   }
 
   const gamePathFileNames = fs.readdirSync(gamePath);
@@ -65,7 +89,7 @@ const openGameInstaller = async (
   );
 
   if (gamePathExecutableFiles.length === 1) {
-    return executeGameInstaller(
+    return await executeGameInstaller(
       path.join(gamePath, gamePathExecutableFiles[0])
     );
   }
--- src/main/events/library/open-game.ts
+++ src/main/events/library/open-game.ts
@@ -3,6 +3,8 @@ import { gameRepository } from "@main/repository";
 import { registerEvent } from "../register-event";
 import { shell } from "electron";
 import { parseExecutablePath } from "../helpers/parse-executable-path";
+import { spawnSync, exec } from "node:child_process";
+import fs from "node:fs";
 
 const openGame = async (
   _event: Electron.IpcMainInvokeEvent,
@@ -13,7 +15,39 @@ const openGame = async (
 
   await gameRepository.update({ id: gameId }, { executablePath: parsedPath });
 
-  shell.openPath(parsedPath);
+  if (process.platform === "win32") {
+	shell.openPath(parsedPath);
+    return true;
+  }
+
+  if (spawnSync("which", ["wine"]).status === 0) {
+    exec(`wine "${parsedPath}"`);
+    return true;
+  }
+
+	const STEAM_COMPAT_CLIENT_INSTALL_PATH = `${process.env.HOME}/.local/share/Steam`;
+	if (fs.existsSync(STEAM_COMPAT_CLIENT_INSTALL_PATH)) {
+		const [ ver ] = fs.readdirSync(`${STEAM_COMPAT_CLIENT_INSTALL_PATH}/steamapps/common/`).filter(proton => proton.startsWith('Proton '));
+		if (ver) {
+			const STEAM_COMPAT_MOUNTS = `${STEAM_COMPAT_CLIENT_INSTALL_PATH}/steamapps/common/${ver}`;
+			const STEAM_COMPAT_DATA_PATH = `${process.env.HOME}/.config/hydra/compatdata/${gameId}`;
+			const WINEPREFIX = `${STEAM_COMPAT_DATA_PATH}/pfx`;
+			if (!fs.existsSync(`${WINEPREFIX}/drive_c`))
+				fs.mkdirSync(`${WINEPREFIX}/drive_c`, { recursive: true });
+			exec(`"${STEAM_COMPAT_MOUNTS}/proton" run "${parsedPath}"`, {
+				env: {
+					...process.env,
+					STEAM_COMPAT_CLIENT_INSTALL_PATH,
+					STEAM_COMPAT_MOUNTS,
+					STEAM_COMPAT_DATA_PATH,
+					WINEPREFIX
+				}
+			});
+			return true;
+		}
+	}
+
+  return false;
 };
 
 registerEvent("openGame", openGame);
